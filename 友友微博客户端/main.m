9f08fe38dbbb2bc8993f85d82265ba3f82860d1d
LockeJiang <jiangjian115@163.com>
2014-02-16 15:43:42 +0800

    Initial Commit

---
+++ 友友微博客户端/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  友友微博客户端
+//
+//  Created by Jiang Jian on 14-2-16.
+//  Copyright (c) 2014年 Locke Jiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "lockeAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([lockeAppDelegate class]));
+    }
+}
---
+++ 友友微博客户端Tests/_______Tests.m
@@ -0,0 +1,34 @@
+//
+//  _______Tests.m
+//  友友微博客户端Tests
+//
+//  Created by Jiang Jian on 14-2-16.
+//  Copyright (c) 2014年 Locke Jiang. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface _______Tests : XCTestCase
+
+@end
+
+@implementation _______Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
